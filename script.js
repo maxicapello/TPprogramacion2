e18260fbe9a5a51ac98c8e3cca387638013a73b3
Ludex85 <maximiliano_capello@hotmail.com>
2024-05-13 05:44:03 -0300

    added the alpha of how products will look

---
+++ script.js
@@ -0,0 +1,29 @@
+let slideIndex = 0;
+const slides = document.querySelectorAll('.slider img');
+
+function showSlide(index) {
+    if (index >= slides.length) {
+        slideIndex = 0;
+    } else if (index < 0) {
+        slideIndex = slides.length - 1;
+    }
+
+    slides.forEach(slide => {
+        slide.style.display = 'none';
+    });
+
+    slides[slideIndex].style.display = 'block';
+}
+
+function nextSlide() {
+    slideIndex++;
+    showSlide(slideIndex);
+}
+
+function prevSlide() {
+    slideIndex--;
+    showSlide(slideIndex);
+}
+
+// Auto-avance de diapositivas cada 3 segundos
+setInterval(nextSlide, 3000);
